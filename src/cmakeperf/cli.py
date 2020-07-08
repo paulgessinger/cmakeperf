1b0888ccdfadeb71cb0cb3b3c7d85d6035b438f6
Paul Gessinger <hello@paulgessinger.com>
2020-07-08 14:02:05 +0200

    mt

--- src/cmakeperf/cli.py
+++ src/cmakeperf/cli.py
@@ -9,9 +9,47 @@ import json
 import os
 import re
 import io
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import threading
 
 import click
 import psutil
+
+
+def run(command, file, directory, interval, progress):
+  rp = os.path.relpath(file, os.getcwd())
+  
+
+  p = psutil.Popen(command, shell=True, cwd=directory, stdout=sp.PIPE, stderr=sp.STDOUT)
+
+  max_mem = 0
+  start = datetime.now()
+
+  while p.status() in (psutil.STATUS_RUNNING, psutil.STATUS_SLEEPING):
+    mem = 0
+    children = list(p.children(recursive=True))
+    for subp in p.children(recursive=True):
+      try:
+        mem += subp.memory_info().rss
+      except (psutil.NoSuchProcess, psutil.AccessDenied):
+        pass
+
+    # in KB, go to MB
+    mem = mem
+    max_mem = max(mem, max_mem)
+
+    delta = datetime.now() - start
+    if progress:
+      sys.stderr.write(f"{rp}: {mem/1e6:8.2f}M, max: {max_mem/1e6:8.2f}M [{delta.total_seconds():8.2f}s]\r")
+    time.sleep(interval)
+
+  p.wait()
+  if progress:
+    sys.stderr.write("\n")
+
+  delta = datetime.now() - start
+  return file, max_mem, delta
+
 
 
 @click.command()
@@ -19,7 +57,8 @@ import psutil
 @click.option("--output", "-o", default="-", help="Output CSV to file, or stdout (use -)")
 @click.option("--filter", default=".*", help="Filter input files by regex")
 @click.option("--interval", type=float, default=0.5, help="Sample interval")
-def main(compile_db, output, filter, interval):
+@click.option("--jobs", "-j", type=int, default=1)
+def main(compile_db, output, filter, interval, jobs):
 
   regex = re.compile(filter)
   cwd = os.getcwd()
@@ -39,51 +78,36 @@ def main(compile_db, output, filter, interval):
   writer = csv.writer(outstr, delimiter=',')
   writer.writerow(["file", "max_rss", "time"])
   outstr.flush()
+    
+  with ThreadPoolExecutor(jobs) as ex:
+    futures = []
+    try:
+      for item in commands:
+        command = item["command"]
+        file = item["file"]
+        directory = item["directory"]
 
-  try:
-    for item in commands:
-      command = item["command"]
-      file = item["file"]
-      directory = item["directory"]
-
-      rp = os.path.relpath(file, cwd)
-      
-      if not regex.match(file):
-        continue
-
-      os.chdir(directory)
-
-      p = psutil.Popen(command, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT)
-
-      max_mem = 0
-      start = datetime.now()
-
-      while p.status() in (psutil.STATUS_RUNNING, psutil.STATUS_SLEEPING):
-        mem = 0
-        children = list(p.children(recursive=True))
-        for subp in p.children(recursive=True):
-          try:
-            mem += subp.memory_info().rss
-          except (psutil.NoSuchProcess, psutil.AccessDenied):
-            pass
-
-        # in KB, go to MB
-        mem = mem
-        max_mem = max(mem, max_mem)
-
-        delta = datetime.now() - start
-        sys.stderr.write(f"{rp}: {mem/1e6:8.2f}M, max: {max_mem/1e6:8.2f}M [{delta.total_seconds():8.2f}s]\r")
-        time.sleep(interval)
-
-      p.wait()
-      sys.stderr.write("\n")
-
-      delta = datetime.now() - start
-      writer.writerow([file, max_mem, delta.total_seconds()])
-      outstr.flush()
-  finally:
-    if outstr != sys.stdout:
-      outstr.close()
+        if not regex.match(file):
+          continue
+  
+        futures.append(ex.submit(run, command, file, directory, interval, jobs==1))
+  
+      try:
+        for f in as_completed(futures):
+            file, max_mem, delta = f.result()
+            writer.writerow([file, max_mem, delta.total_seconds()])
+            outstr.flush()
+            if jobs > 1:
+              rp = os.path.relpath(file, os.getcwd())
+              sys.stderr.write(f"{rp}: max: {max_mem/1e6:8.2f}M [{delta.total_seconds():8.2f}s]\n")
+      finally:
+        if outstr != sys.stdout:
+          outstr.close()
+    except KeyboardInterrupt:
+      print("Ctrl+C")
+      for f in futures:
+          f.cancel()
+      ex.shutdown()
 
 
 if "__main__" == __name__:
